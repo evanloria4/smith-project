4ab4af00fdf3ede4004e10fcf0f90aec7f6f8f64
evan.loria4 <evan.loria4@gmail.com>
2025-05-09 11:31:37 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+const CLIENT_DIR = path.resolve(__dirname, 'src', 'client');
+
+module.exports = {
+  entry: path.resolve(CLIENT_DIR, 'index.tsx'),
+  output: {
+    path: DIST_DIR,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },devServer: {
+    static: {
+      directory: DIST_DIR, // Serve from dist
+    },
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    port: 4000,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(m?js)$/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|mp3|aac|ogg|ico)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-typescript',
+                '@babel/preset-react',
+              ]
+            }
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          'postcss-loader'
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(CLIENT_DIR, 'index.html'),
+      filename: 'index.html',
+      inject: true
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css'],
+    alias: {
+      '@': SRC_DIR,
+    },
+    modules: [
+      'node_modules',
+      SRC_DIR,
+    ],
+    fallback: {
+      "crypto": false
+    }
+  }
+};
